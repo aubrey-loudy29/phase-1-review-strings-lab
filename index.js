c5700131809870d61fc1f0ad597a98253af929c4
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-21 11:42:25 -0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser ='Aubrey Loudy';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
